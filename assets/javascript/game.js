b75a58d70c5fbaf2cb4391f4ba215098cc87ed09
Zaria Mateen <zmateen43@gmail.com>
2018-05-08 14:58:30 -0400

    <COOMENT>

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+var wins = 0;
+var losses = 0;
+var numberGuess = 10;
+var guessChoices = [];
+
+object.onkeyup = function (myScript) {
+    var x = event.key;
+    var y = computerChoices[math.floor(math.random() * computerChoices.length)];
+    var z = letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+}
